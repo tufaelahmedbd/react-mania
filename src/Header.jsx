9c312bbae886b8e99d9cd6184c07ec981ef47c76
tufael-ahmed <tufaelahmed1110@gmail.com>
2026-04-20 17:58:34 +0200

    hook basic

---
+++ src/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://www.designmantic.com/logo-images/171406.png?company=Company%20Name&keyword=food&slogan=&verify=1"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/RestaurantCard.jsx
@@ -0,0 +1,46 @@
+import { CDN_URL } from "../utils/constants";
+import React from "react";
+
+const RestaurantCard = (props) => {
+  const { restaurantData } = props;
+
+  const {
+    cloudinaryImageId,
+    name,
+    areaName,
+    avgRating,
+    cuisines,
+    costForTwo,
+    deliveryTime,
+  } = restaurantData?.info;
+
+  return (
+    <div className="restaurant-card">
+      <img
+        src={CDN_URL + cloudinaryImageId}
+        alt={name}
+        className="restaurant-logo"
+      />
+      <div className="restaurant-details">
+        <h3 className="restaurant-name">
+          {name.slice(0, 22)}
+          {name.length > 22 ? "..." : ""}
+        </h3>
+        <div className="esa-rating">
+          <h4 className="rating">
+            <span>{avgRating}</span>
+          </h4>
+          <h4>{costForTwo}</h4>
+          <h4>{deliveryTime} mins</h4>
+        </div>
+        <p className="cousine">
+          {cuisines.join(", ").slice(0, 30)}
+          {cuisines.join(", ").length > 30 ? "..." : ""}
+        </p>
+        <p className="location">{areaName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCard;
